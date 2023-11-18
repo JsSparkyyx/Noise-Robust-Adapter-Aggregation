8d55db9a53d49cbb1a2880a79cb3c31d01e8b78e
JsSparkyyx <y.yuxuan@chenguang02.engr.wustl.edu>
2023-11-18 17:32:17 -0600

    1

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 import random
 import numpy as np
 import torch
@@ -8,13 +8,19 @@ def k_split(num_clients,num_error_clients,dataset):
     for i in range(num_clients):
         subdata = dataset.shard(num_clients,i)
         if i < num_error_clients:
-            random.shuffle(subdata['target'])
+            target = subdata['target']
+            random.shuffle(target)
+            source = subdata['source']
+            subdata = Dataset.from_dict({'source':source, 'target':target})
         data.append(subdata)
     return data
 
 def split_data(args):
     if args.dataset == 'glue':
         dataset = load_dataset("JsSparkYyx/NLP524", args.task).shuffle(seed=args.seed)
+    if args.dataset == 'bigbench':
+        dataset = load_dataset("tasksource/bigbench", args.task).shuffle(seed=args.seed)
+        dataset.rename_columns({'inputs':'source','targets':'target'})
     train_ds = k_split(args.num_clients,args.num_error_clients,dataset['train'])
     test_ds = k_split(args.num_clients,args.num_error_clients,dataset['test'])
     valid_ds = k_split(args.num_clients,args.num_error_clients,dataset['valid'])
