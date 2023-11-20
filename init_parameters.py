4186c9a52b0b0e1bbca96acb7255d511eeda95f4
JsSparkyyx <y.yuxuan@chenguang02.engr.wustl.edu>
2023-11-19 18:55:01 -0600

    update init

--- init_parameters.py
+++ init_parameters.py
@@ -3,7 +3,7 @@ import argparse
 def init_parameters():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, choices=['glue','bigbench'], default='glue')
-    parser.add_argument('--task', type=str, choices=['mnli','qnli','sst2','qqp','elementary_math_qa','cryptonite','intersect_geometry','list_functions'], default='mnli')
+    parser.add_argument('--task', type=str, choices=['mnli','qnli','sst2','qqp','elementary_math_qa','tracking_shuffled_objects','cryptonite','intersect_geometry','list_functions'], default='mnli')
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--num_clients', type=int, default=10)
     parser.add_argument('--num_error_clients', type=int, default=3)
