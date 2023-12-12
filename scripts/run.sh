35c3abbe56dd75f69a6343816023960931f6689d
JsSparkyyx <61544815+JsSparkyyx@users.noreply.github.com>
2023-12-12 15:40:06 -0600

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,6 @@
-python main.py --dataset glue --task qqp --num_clients 3
+python main.py --dataset glue --task qqp 
 python main.py --dataset glue --task mnli
 python main.py --dataset glue --task qnli
+python main.py --dataset glue --task sst2
+python main.py --dataset bigbench --task intersect_geometry
+python main.py --dataset bigbench --task list_functions
