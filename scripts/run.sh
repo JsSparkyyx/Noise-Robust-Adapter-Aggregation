427855d0e3e95039fa9356d2066c155c2517fcb1
JsSparkyyx <61544815+JsSparkyyx@users.noreply.github.com>
2023-12-12 15:40:31 -0600

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,3 +4,5 @@ python main.py --dataset glue --task qnli
 python main.py --dataset glue --task sst2
 python main.py --dataset bigbench --task intersect_geometry
 python main.py --dataset bigbench --task list_functions
+python main.py --dataset bigbench --task elementary_math_qa 
+python main.py --dataset bigbench --task cryptonite 
