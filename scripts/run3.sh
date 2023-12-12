11d6fd571f09cdfa21b6e9d491974c156d93ad0d
JsSparkyyx <61544815+JsSparkyyx@users.noreply.github.com>
2023-12-12 15:39:40 -0600

    Update run3.sh

--- scripts/run3.sh
+++ scripts/run3.sh
@@ -1,3 +1,3 @@
-python main.py --dataset bigbench --task intersect_geometry --num_clients 3
-python main.py --dataset glue --task sst2 --num_clients 3
+python main.py --dataset bigbench --task intersect_geometry
+python main.py --dataset glue --task sst2
 python main.py --dataset bigbench --task list_functions
